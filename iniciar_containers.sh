f64ff2777397a887b5931054fb2c42d59ab31112
rfavaronl <rfavaronl@gmail.com>
2025-03-27 22:34:38 -0300

    Update iniciar_containers.sh

--- iniciar_containers.sh
+++ iniciar_containers.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+#Instalar docker
+sudo apt update
+sudo apt install docker.io -y
+
+#Iniciar containers mysql e php-apache
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=Senha123 -dt mysql
+docker run --name webserver -dt -p 80:80 --mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
